bcd84731746df89438ce06dd4ab77e326c028c1e
Kezia Natalia <kezia.natalia@ui.ac.id>
2024-05-22 18:38:12 +0700

    initial commit

---
+++ src/services/inferenceService.js
@@ -0,0 +1,39 @@
+const tf = require('@tensorflow/tfjs-node');
+const InputError = require('../exceptions/InputError');
+ 
+async function predictClassification(model, image) {
+
+    try {
+        const tensor = tf.node
+            .decodeJpeg(image)
+            .resizeNearestNeighbor([224, 224])
+            .expandDims()
+            .toFloat()
+
+        const prediction = model.predict(tensor);
+        const score = await prediction.data();
+        const confidenceScore = Math.max(...score) * 100;
+
+        const isCancer = confidenceScore > 50;
+        const classes = ['Non-Cancer', 'Cancer'];
+    
+        // const classResult = tf.argMax(prediction, 1).dataSync()[0];
+        const label = classes[isCancer ? 1 : 0];
+
+        let explanation, suggestion;
+
+        if (isCancer){
+            explanation = 'Terdeteksi kanker'
+            suggestion = 'Segera berkonsultasi ke dokter!'
+        } else {
+            explanation = 'Tidak terdeteksi kanker'
+            suggestion='Anda sehat!';
+        }
+
+        return { confidenceScore, label, explanation, suggestion }; 
+    } catch (error) {
+        throw new InputError('Terjadi kesalahan dalam melakukan prediksi')
+    }
+}
+
+module.exports = predictClassification;
